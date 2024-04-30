0121e768e05e99727a55b82c115c75130c34a522
yusraahmed3 <yusraa190@gmail.com>
2024-04-30 14:06:06 +0300

    padding fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import Background from '../assets/images/background.png'
 
 function Home() {
   return (
-    <div className="w-full h-full flex lg:items-center p-10">
-      <div className="w-full flex flex-col lg:flex-row items-center justify-between md:justify-evenly lg:justify-between">
+    <div className="flex h-full w-full p-20 lg:items-center lg:p-10">
+      <div className="flex w-full flex-col items-center justify-between md:justify-evenly lg:flex-row lg:justify-between">
       <Logo/>
       <Links/>
       </div>
