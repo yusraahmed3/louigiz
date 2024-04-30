f54d1dc9329259c3d4d00febd9221c8c91ba44e5
yusraahmed3 <yusraa190@gmail.com>
2024-04-30 14:12:37 +0300

    padding and image fix

--- src/components/PagesCss.jsx
+++ src/components/PagesCss.jsx
@@ -1,2 +1,2 @@
-const PageCss = 'pt-5 px-8 md:px-14 lg:px-40  pb-32 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10 md:gap-8 place-items-center';
+const PageCss = 'pt-5 px-8 md:px-14 lg:px-40  pb-32 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-12 md:gap-10 place-items-center';
 export default PageCss;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Background from '../assets/images/background.png'
 
 function Home() {
   return (
-    <div className="flex h-full w-full p-20 lg:items-center lg:p-10">
+    <div className="flex h-full w-full p-24 lg:items-center lg:p-10">
       <div className="flex w-full flex-col items-center justify-between md:justify-evenly lg:flex-row lg:justify-between">
       <Logo/>
       <Links/>
