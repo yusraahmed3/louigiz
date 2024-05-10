ee66f6786b231603c900519db4d7c2096cbc8003
yusraahmed3 <yusraa190@gmail.com>
2024-05-10 20:22:20 +0300

    fix

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { logEvent } from '../analytics'
+import { logEvent } from '/../analytics'
 const ImageBox = ({image, alt, key, title, link}) => {
   return (
     <div className='flex flex-col justify-center'>
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-import { initGA } from './analytics';
+import { initGA } from '/./analytics';
 
 initGA()
 
